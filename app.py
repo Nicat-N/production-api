19bddf387ef85622393742f85c7c02b382e84fd1
Nicat-N <nicatniftaliyev00@gmail.com>
2025-03-29 18:21:01 +0000

    scommit

--- app.py
+++ app.py
@@ -7,7 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources = {r"/api/news": {
+    "origins": ["https://github.com/Nicat-N"]
+    "methods": ["GET"]
+}})
 
 def get_db_connection():
 
